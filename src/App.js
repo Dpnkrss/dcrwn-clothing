c361bbc80ab7e389d69e26b6e138fda5d262f253
Deepankar <deepankarsalhan@gmail.com>
2023-03-31 21:10:59 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import { Routes, Route,  } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from "./routes/home/home.component";
+import Checkout from './routes/checkout/checkout.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+
+
+const App = () => {
+
+return (
+  <Routes>
+  <Route path= '/' element={<Navigation />}>
+  <Route index element={<Home />} />
+  <Route path='shop' element={<Shop />} />
+  <Route path='auth' element={<Authentication />} />
+  <Route path='checkout' element={<Checkout />} />
+  </Route>
+</Routes>
+);
+
+};
+
+export default App;
